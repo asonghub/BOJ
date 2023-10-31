52220901546ec0a9a531574bc0ea149aa043c302
Asong Ma <136426795+asonghub@users.noreply.github.com>
2023-10-31 21:06:07 +0900

    Bronze5 별

---
+++ Bronze/Bronze V/B5_2438.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * B5_2438
+ */
+public class B5_2438 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt() ;
+
+        for(int i =1; i <= n ; i++){
+            for(int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        sc.close();
+    }
+}
