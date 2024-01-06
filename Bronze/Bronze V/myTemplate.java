2bdfb61740320e6b8fe5d9c464b628ae21e3d12d
Asong Ma <136426795+asonghub@users.noreply.github.com>
2024-01-06 23:03:27 +0900

    문제풀이 템플릿

---
+++ Bronze/Bronze V/myTemplate.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class myTemplate {
+
+    private static BufferedReader in;
+    private static BufferedWriter out;
+    
+    public static void main(String[] args) throws IOException {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+    }
+}
