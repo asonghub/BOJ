aa347ef9999aa523f408b1715f3b94066feb0284
Asong Ma <136426795+asonghub@users.noreply.github.com>
2024-01-21 22:41:20 +0900

    B5_2475

--- Bronze/Bronze V/B5_2475.java
+++ Bronze/Bronze V/B5_2475.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class B5_2475 {
     private static BufferedReader in;
     private static BufferedWriter out;
